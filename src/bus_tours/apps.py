6678d6d485ddab776a40914be82b41e739ea51f1
Mirlan uulu Chyngyz <chyngyz@1906041-hc59128.twc1.net>
2024-01-15 13:08:29 +0300

    commity

--- src/bus_tours/apps.py
+++ src/bus_tours/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BusToursConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'src.bus_tours'
+    verbose_name = 'Экскурсионные, авторские туры'
